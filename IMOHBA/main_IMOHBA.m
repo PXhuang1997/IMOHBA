2d56f88c3cfa9626d1ee50536479afa1ced524de
PXhuang1997 <90662894+PXhuang1997@users.noreply.github.com>
2023-08-20 10:15:45 +0800

    Update main_IMOHBA.m

--- IMOHBA/main_IMOHBA.m
+++ IMOHBA/main_IMOHBA.m
@@ -5,9 +5,9 @@ clear;clc
 fname={'IZDT1';'IZDT2';'IZDT3';'IZDT4';'IZDT6';'IDTLZ1';'IDTLZ2';'IDTLZ3';'IDTLZ4';'IDTLZ5';'IDTLZ6';'IMMF1';'IMMF2';
     'IMMF3';'IMMF4';'IMMF5';'IMMF6';'IMMF7';'IMMF8'};
 num=length(fname); % number of test function
-for i=1:1
+for i=15:15
     name=fname{i,:};
-    num_of_runs=2;          %number of run times
+    num_of_runs=20;          %number of run times
     pop=100;gen=400;         %Population size and Total number of generations
     for runs = 1:num_of_runs 
         fprintf('number of run times:%d\n',runs)
